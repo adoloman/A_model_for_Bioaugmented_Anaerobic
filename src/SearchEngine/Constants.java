5f6c5086e288a90e33f69a2530311c1c3d37dffe
Yehor Pererva <Pererva@users.noreply.github.com>
2019-02-18 14:22:49 -0700

    minor changes

--- src/SearchEngine/Constants.java
+++ src/SearchEngine/Constants.java
@@ -1,8 +1,12 @@
 package SearchEngine;
 
 public class Constants {
-	//Give the path based on where the files are located
-	public static  String OUTPUT_PATH="C:\\iDynoMICs\\results\\Out.txt";
-	public static  String RESULT_PATH="C:\\iDynoMICs\\results";
+	// Give the path based on where the files are located
+	// Location of protocol (as .xml-file)
 	public static  String XML_PATH="C:\\iDynoMICs\\protocols\\test_protocol.xml";
+	// Folder for simulation results
+	// Inside of this folder a sub-folder will be created as "protocol_name"_("timestamp")
+	public static  String RESULT_PATH="C:\\iDynoMICs\\results";
+
+	public static  String OUTPUT_PATH="C:\\iDynoMICs\\results\\Out.txt";
 }
