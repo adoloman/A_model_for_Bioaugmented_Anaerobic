4215b0e7915705d1d04a621af200d0f2c7cc8926
Yehor Pererva <Pererva@users.noreply.github.com>
2019-07-04 13:01:22 -0600

    testing with Linux machine

--- launch_scripts/launch.sh
+++ launch_scripts/launch.sh
@@ -3,9 +3,20 @@
 # sudo dnf -y install java-1.8.0-openjdk-devel
 java -version
 
-DYNO_PATH=$(dirname $(dirname $(realpath $0)))
-echo iDynoMICs project is located at "$DYNO_PATH"
+# java 8 in Fedora is located at: /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.fc30.x86_64/bin/java
+# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0
+# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0-openjdk
 
+# java 8 in Ubuntu is located at : /usr/lib/jvm/java-8-openjdk-amd64/bin/java
+# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0-openjdk-amd64
+JAVA_FOLDER=$(find '/usr/lib' -name "*java-1.8.0*" -type d)
+echo $JAVA_FOLDER
+
+JAVA_8_EXEC=$JAVA_FOLDER'/bin/java'
+JAVAC_8_EXEC=$JAVA_FOLDER'/bin/javac'
+
+# root folder of iDynoMICs
+DYNO_PATH=$(dirname $(dirname $(realpath $0)))
 SRC_PATH=$DYNO_PATH'/src'
 
 # Collecting all path needed to build / compile / run
@@ -18,11 +29,3 @@ for jar_file in $DYNO_PATH'/src/lib/'*.jar; do
 done
 
 cd $DYNO_PATH
-# java 8 in Fedora is located at: /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.fc30.x86_64/bin/java
-# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0
-# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0-openjdk
-
-# java 8 in Ubuntu is located at : /usr/lib/jvm/java-8-openjdk-amd64/bin/java
-# the folder itself is available with a shortcut via /usr/lib/jvm/java-1.8.0-openjdk-amd64
-JAVA_FOLDER=$(find '/usr/lib' -name "*java-1.8.0*" -type d)
-echo $JAVA_FOLDER
--- src/SearchEngine/Constants.java
+++ src/SearchEngine/Constants.java
@@ -3,15 +3,14 @@ package SearchEngine;
 public class Constants {
 	// Give the path based on where the files are located
 	// Location of protocol (as .xml-file)
-	public static  String XML_PATH="C:\\iDynoMICs\\protocols\\test_protocol.xml";
+	// public static  String XML_PATH="C:\\iDynoMICs\\protocols\\test_protocol.xml";
 	// Folder for simulation results
 	// Inside of this folder a sub-folder will be created as "protocol_name"_("timestamp")
-	public static  String RESULT_PATH="C:\\iDynoMICs\\results";
-
-	public static  String OUTPUT_PATH="C:\\iDynoMICs\\results\\Out.txt";
+	// public static  String RESULT_PATH="C:\\iDynoMICs\\results";
+	// public static  String OUTPUT_PATH="C:\\iDynoMICs\\results\\Out.txt";
 	// In case of running under Linux comment variables above and edit variables below:
-	// public static  String XML_PATH="/iDynoMICs/protocols/test_protocol.xml";
-	// public static  String RESULT_PATH="/iDynoMICs/results";
-	// public static  String OUTPUT_PATH="/iDynoMICs/results/Out.txt";
+	public static  String XML_PATH="/iDynoMICs/protocols/test_protocol.xml";
+	public static  String RESULT_PATH="/iDynoMICs/results";
+	public static  String OUTPUT_PATH="/iDynoMICs/results/Out.txt";
 
 }
