6c2a6c227b35769f53a4de4e7d0fe439d1bc897a
Yehor Pererva <Pererva@users.noreply.github.com>
2019-07-11 15:36:59 -0600

    Update launch_via_slurm.sh

--- launch_scripts/launch_via_slurm.sh
+++ launch_scripts/launch_via_slurm.sh
@@ -10,16 +10,25 @@
 # local copy of software will be running at /scratch/iDynoMICs
 # /iDynoMICs/src/SearchEngine/Constans.java contains already fixed paths for final location of protocols, etc.
 #
+# Algorithm:
+# 1) Identify location of script and iDynoMICs distro
+# 2) Check if computational node contains any leftovers from previous runs
+# 3) Copy from main storage to a local storage of computational node
+# 4) Run at computational Node
+# 5) Copy calculation results from compute node to centralized storage
+# 6) Purge local distro
 
-# getting location of where script was started (!)
-# echo $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
-
+# Check if script is launched from SLURM or BASH
+# SLURM creates variable $SLURM_JOB_ID, thus it shouldn't be empty
 if [ -n $SLURM_JOB_ID ];  then
+	# get original location of launching script
 	THEPATH=$(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
 else
+	# otherwise it is started from bash and obtain location in normal way
 	THEPATH=$(realpath $0)
 fi
 
+# get location of distro at shared storage
 DYNO_SHARED_FOLDER=$(dirname $(dirname "${THEPATH}"))
 
 SCRATCH_FOLDER='/scratch'
@@ -30,20 +39,15 @@ DYNO_FOLDER_NAME=$(basename $DYNO_SHARED_FOLDER)
 DYNO_LOCAL_FOLDER=$SCRATCH_FOLDER/$DYNO_FOLDER_NAME
 LAUNCH_SCRIPT=$DYNO_LOCAL_FOLDER/$LAUNCH_RELATIVE_LOCATION
 
-Echo "Purging local copy from previous run (if exists)"
+# Purging local copy from previous run (if exists)
 rm -rf ${DYNO_LOCAL_FOLDER}
-
 # Copying the distro from shared folder
 cp -r ${DYNO_SHARED_FOLDER} ${DYNO_LOCAL_FOLDER}
-
 # running calculations
 sh $LAUNCH_SCRIPT
-
+# create folder on central storage for transfering computation results
 mkdir ${STORAGE_FOLDER} -p
+# copy results
 cp -R ${DYNO_LOCAL_FOLDER}/results/* ${STORAGE_FOLDER}
-
+# prge local copy of distro
 rm -rf "$DYNO_LOCAL_PATH"
-
-
-
-
